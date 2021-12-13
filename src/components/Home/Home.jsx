954bf23097292426c31ade015403dc9307bd0a29
Isiah Smith <isiahthomassmith@gmail.com>
2021-12-13 14:05:42 -0600

    add challenge to imports on root reducer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 function Home() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const challenge = useSelector((store) => store.challenge);
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_CHALLENGES' });
+}, []);
+
+
   return (
     <div className="container">
       <h2>Welcome To The Home Page!</h2>
