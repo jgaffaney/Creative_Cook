e8569bae9f007aeeb14cd7a75f34cb8b7f20e7bf
Isiah Smith <isiahthomassmith@gmail.com>
2021-12-13 11:01:18 -0600

    challenge.router page made

---
+++ server/routes/challenge.router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/challenge', (req, res) => {
+  // GET route code here
+});
+
+/**
+ * POST route template
+ */
+router.post('/challenge', (req, res) => {
+  // POST route code here
+});
+
+module.exports = router;
