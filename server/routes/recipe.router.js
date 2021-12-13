cd478b3b6a8d4a83bc6240be2ba214af91396755
Jordan Ponto <jponto93@gmail.com>
2021-12-13 11:22:05 -0600

    recipe route  and saga created

--- server/routes/recipe.router.js
+++ server/routes/recipe.router.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const axios = require('axios');
 
 /**
  * GET route template
  */
 router.get('/', (req, res) => {
   // GET route code here
+  axios.get(`https://api.edamam.com/api/recipes/v2?app_key=${process.env.app_key}&app_id=${process.env.app_id}&type=public&q=chicken`)
+    .then(response => {
+        console.log('response is', response);
+        res.send(response.data);
+    })
+    .catch(err => {
+        console.log('err in recipe GET', err);
+        res.sendStatus(500);
+    })
 });
 
 /**
