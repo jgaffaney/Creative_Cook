e3de38b0b7a49f18f996f5393a6ef950e82a8672
Jordan Ponto <jponto93@gmail.com>
2021-12-13 14:13:59 -0600

    recipe api route working

--- server/routes/recipe.router.js
+++ server/routes/recipe.router.js
@@ -12,8 +12,7 @@ router.get('/', (req, res) => {
   console.log('app_id', process.env.app_id);
   
   
-  axios.get(`https://api.edamam.com/api/recipes/v2?app_key=${process.env.app_key}&app_id=${process.env.app_id}&type=public&q=chicken`)
-//   axios.get(`https://api.edamam.com/api/recipes/v2?app_key=8c0986b8e9521ffff067fac51e04341b&app_id=3b8d7964&type=public&q=chicken`)
+  axios.get(`https://api.edamam.com/api/recipes/v2?app_key=${process.env.APP_KEY}&app_id=${process.env.APP_ID}&type=public&q=chicken`)
     .then(response => {
         console.log('response is', response);
         res.send(response.data);
