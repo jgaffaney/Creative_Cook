8fb3ebe09a08de50c1d87148384bb355cfb5228c
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-12-13 15:08:30 -0600

    working on table

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -1,5 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
+import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector, useDispatch } from 'react-redux';
+import { useTable, useGlobalFilter, useFilters } from 'react-table';
+import { COLUMNS } from './columns';
+import './table.css';
+import { GlobalFilter } from '../../components/GlobalFilter/GlobalFilter';
+
+
 
 function Ingredients() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -8,15 +15,77 @@ function Ingredients() {
 
   const dispatch = useDispatch();
 
+  const columns = useMemo(() => COLUMNS, []);
+  const ingredientList = useMemo(() => ingredients, []);
+
+
   useEffect(() => {
-    dispatch({type: 'FETCH_INGREDIENTS'})
+    dispatch({ type: 'FETCH_INGREDIENTS' })
   }, [])
 
+  const {
+    getTableProps,
+    // getTableBodyProps,
+    headerGroups,
+    // footerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = useTable({
+    columns,
+    ingredientList,
+  },
+    useFilters,
+    useGlobalFilter)
+
+  const { globalFilter } = state;
+
   return (
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter}/>
     <div className="container">
       <h2>Welcome To The Ingredients Page!</h2>
-        <p>{ingredients[0].name}</p>
     </div>
+    <table {...getTableProps()}>
+            <thead>
+                {
+                    headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {
+                                headerGroup.headers.map(column => (
+                                    <th {...column.getHeaderProps()}>
+                                        {column.render('Header')}
+                                        <div>
+                                            {column.canFilter ? column.render('Filter') : null }
+                                        </div>
+                                    </th>
+                                ))
+                            }
+                        </tr>
+                    ))
+                }
+            </thead>
+            <tbody {...getTableProps()}>
+                {
+                    rows.map((row) => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {
+                                    row.cells.map((cell) => {
+                                        return (
+                                            <td {...cell.getCellProps()}>
+                                                {cell.render('Cell')}
+                                            </td>
+                                        )
+                                    })}
+                            </tr>
+                        )
+                    })}
+            </tbody>
+          </table>
+    </>
   );
 }
 
