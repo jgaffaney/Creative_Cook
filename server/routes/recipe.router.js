ce87b76bca136b4de8a37e0819fa6efe2284a21a
Jordan Ponto <jponto93@gmail.com>
2021-12-13 14:10:36 -0600

    .env added

--- server/routes/recipe.router.js
+++ server/routes/recipe.router.js
@@ -8,7 +8,12 @@ const axios = require('axios');
  */
 router.get('/', (req, res) => {
   // GET route code here
+  console.log('app_key', process.env.app_key);
+  console.log('app_id', process.env.app_id);
+  
+  
   axios.get(`https://api.edamam.com/api/recipes/v2?app_key=${process.env.app_key}&app_id=${process.env.app_id}&type=public&q=chicken`)
+//   axios.get(`https://api.edamam.com/api/recipes/v2?app_key=8c0986b8e9521ffff067fac51e04341b&app_id=3b8d7964&type=public&q=chicken`)
     .then(response => {
         console.log('response is', response);
         res.send(response.data);
