ebc78af49d61adaf5a288c16cdd6538799435f36
Isiah Smith <isiahthomassmith@gmail.com>
2021-12-13 11:23:15 -0600

    challenge POST in progress, missing type

--- server/routes/challenge.router.js
+++ server/routes/challenge.router.js
@@ -9,11 +9,22 @@ router.get('/challenge', (req, res) => {
   // GET route code here
 });
 
-/**
- * POST route template
- */
+// Challenge POST route
 router.post('/challenge', (req, res) => {
-  // POST route code here
-});
+    const queryText = `
+        INSERT INTO "feed_content" ("id", "description", "combo_id")
+        VALUES ($1, $2, $3);
+        `;
+    values = [req.body.id, req.body.description, req.body.combo_id]
+    console.log('values are', values);
+    pool.query(queryText, values)
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch(err => {
+            console.log('Error in Challenge POST', err);
+            res.sendStatus(500);
+        })
+}); // End POST
 
 module.exports = router;
