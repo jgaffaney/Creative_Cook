b16443d0780c67deb3b166b9ba24ad6d7db0e463
Isiah Smith <isiahthomassmith@gmail.com>
2021-12-14 10:32:59 -0600

    combo reducer made

---
+++ src/redux/reducers/combo.reducer.js
@@ -0,0 +1,12 @@
+const combo = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_COMBO':
+            return action.payload;
+        case 'LOGOUT':
+            return [];
+        default:
+            return state;
+    }
+};
+
+export default combo;
