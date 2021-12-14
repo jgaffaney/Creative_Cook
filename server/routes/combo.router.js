5fd3a50b281eb240ea68f3cf6f4beea81bd16517
Isiah Smith <isiahthomassmith@gmail.com>
2021-12-14 10:22:01 -0600

    combo router and GET route made

---
+++ server/routes/combo.router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+// Combo GET route
+router.get('/', (req, res) => {
+    const queryText = `
+        SELECT * FROM "combos"
+        ORDER BY "id";
+        `;
+    pool.query(queryText)
+        .then(result => {
+            res.send(result.rows); // Contains all combos
+        })
+        .catch(err => {
+            console.log('Error in Challenge GET', err);
+            res.sendStatus(500);
+        })
+}); // End GET
