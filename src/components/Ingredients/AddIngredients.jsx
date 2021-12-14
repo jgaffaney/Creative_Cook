d67138be51800a1a11d9233c06192d3fa34bb094
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-12-14 09:59:24 -0600

    addingredient stubbed in

--- src/components/Ingredients/AddIngredients.jsx
+++ src/components/Ingredients/AddIngredients.jsx
@@ -1,2 +1,17 @@
 import { FormControl, Box, TextField } from "@mui/material";
 
+export default function AddIngredients() {
+
+    return(
+        <div>
+            <h2>Add New Ingredient</h2>
+            <Box>
+                <FormControl>
+                    
+                </FormControl>
+            </Box>
+        </div>
+        
+
+    )
+}
