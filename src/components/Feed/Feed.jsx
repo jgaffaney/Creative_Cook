5aef4392da2124a674632b0916806a1b6077c593
Isiah Smith <isiahthomassmith@gmail.com>
2021-12-15 14:21:00 -0600

    placeholders for app wide metrics added to admin feed page

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -4,6 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import ComboTool from '../ComboTool/ComboTool';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 
 function Feed() {
   const user = useSelector((store) => store.user);
@@ -28,43 +32,79 @@ function Feed() {
     dispatch({ type: 'ADD_CHALLENGE', payload: newChallenge });
   };
 
-  const sxFeedPage = {
+  const sxFeaturedCombo = {
     display: 'flex',
     flexDirection: 'column',
     mx: 'auto',
-    width: '60%',
+    width: '90%',
   }
 
+  const sxMetrics = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '90%',
+    mx: 'auto',
+    height: 80,
+  }
+
+  const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
   return (
-    <Box sx={sxFeedPage}>
-      <h2>Create Featured Combo</h2>
+    <>
+      <Box sx={sxMetrics}>
+        <Typography>App Wide User Metrics</Typography>
+        <Box sx={sxMetrics}>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <Item>Top 5 Ingredients Used</Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item>% of Used Ingredients by Type</Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item>% of Users by Family Size</Item>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <br />
+      <Box sx={sxFeaturedCombo}>
+      <Typography sx={{textAlign: "center"}}>Create Featured Combo</Typography>
 
 
-      <TextField
-        sx={{ mb: 2 }}
-        id="outlined-basic"
-        variant="outlined"
-        label="Description of Featured Combo"
-        type="text"
-        value={newChallenge.description}
-        onChange={(event) => handlePropertyChange(event, 'description')}
-      />
+        <TextField
+          sx={{ mb: 2, width: '60%', mx: 'auto' }}
+          id="outlined-basic"
+          variant="outlined"
+          label="Description of Featured Combo"
+          type="text"
+          value={newChallenge.description}
+          onChange={(event) => handlePropertyChange(event, 'description')}
+        />
 
-      {/* combo selector */}
-      <ComboTool />
+        {/* combo selector */}
+        <ComboTool />
 
-      {/* list of ingredients from DB */}
-      <ul>
-        {ingredients.map((ingredient) => {
-          return (
-            <li
-              key={ingredient.id}
-              value={ingredient}
-              onClick={() => dispatch({ type: 'SET_COMBO_INGREDIENT', payload: ingredient })}>{ingredient.name}</li>
-          )
-        })}
-      </ul>
-    </Box>
+        {/* list of ingredients from DB */}
+        <ul>
+          {ingredients.map((ingredient) => {
+            return (
+              <li
+                key={ingredient.id}
+                value={ingredient}
+                onClick={() => dispatch({ type: 'SET_COMBO_INGREDIENT', payload: ingredient })}>{ingredient.name}</li>
+            )
+          })}
+        </ul>
+      </Box>
+    </>
   );
 }
 
