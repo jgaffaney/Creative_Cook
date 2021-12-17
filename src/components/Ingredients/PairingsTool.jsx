0b6a5763a1771ead8bec3ca1771f0290894b0f85
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-12-17 14:03:27 -0600

    some styling and initial work on pairings

--- src/components/Ingredients/PairingsTool.jsx
+++ src/components/Ingredients/PairingsTool.jsx
@@ -1,21 +1,31 @@
+import { Button } from '@mui/material';
+import { Box } from '@mui/system';
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
+import IngredientAutocomplete from '../IngredientAutocomplete/IngredientAutocomplete';
+import { sxSearchContainer } from '../Home/Home.style';
 
 const PairingsTool = () => {
 
     const dispatch = useDispatch();
 
     const pairings = useSelector(store => store.pairings);
-    
+
     const [newPairings, setNewPairings] = useState();
 
 
     useEffect(() => {
-        dispatch({type: 'FETCH_PAIRINGS'})
+        dispatch({ type: 'FETCH_PAIRINGS' })
     })
-    return( 
-        <h1>Pairings Tool</h1>
+    return (
+        <>
+            <h1>Pairings Tool</h1>
+            <Box sx={sxSearchContainer}>
+                <IngredientAutocomplete />
+                <Button variant='contained' >Search</Button>
+            </Box>
+        </>
+
     )
 }
 
