589f42be0923010ee4fe6a08ebe120278a600e11
Jordan Ponto <jponto93@gmail.com>
2021-12-21 13:26:35 -0600

    recipe card style good for base. start recipe button works. save recipe does not yet.

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -24,7 +24,6 @@ import { useState } from "react";
 import RecipeListCard from "../RecipeListCard/RecipeListCard";
 import { useHistory } from "react-router-dom";
 
-
 const sxRecipeContainer = {
     display: 'flex',
     flexWrap: 'wrap',
@@ -32,15 +31,14 @@ const sxRecipeContainer = {
     border: '1px solid red',
     gap: 2,
     justifyContent: 'center',
-
 }
 
 const sxRecipeCard = {
     width: 345,
-    height: 450,
-    // overflow: 'scroll',
-    // margin: 2,
+    height: 550,
     backgroundColor: '#fffdfa',
+    elevation: 24,
+    borderRadius: 5,
 }
 
 const sxRecipeImage = {
@@ -58,41 +56,27 @@ const sxCardActions = {
 }
 
 const sxBox = {
-    height: '300px',
+    height: 180,
     overflow: 'scroll',
-    // paddingBottom: 10,
-    // pb: 10
+}
+
+const sxCardContent = {
+    height: 150,
 }
 
 const sxCardTitle = {
     textAlign: 'center',
+    height: 'auto',
 }
+
 
 function RecipeList() {
     const recipes = useSelector(store => store.recipes)
     const history = useHistory();
 
-    // const [expanded, setExpanded] = useState(false);
-
-    // const handleExpandClick = () => {
-    //     setExpanded(!expanded);
-    // }
-
-    //     const ExpandMore = styled((props) => {
-    //         const { expand, ...other } = props;
-    //         return <IconButton {...other} />;
-    //     })(({ theme, expand }) => ({
-    //         transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-    //         marginLeft: 'auto',
-    //         transition: theme.transitions.create('transform', {
-    //             duration: theme.transitions.duration.shortest,
-    //         }),
-    //     }));
-
     return (
         <>
             <h1>RECIPE LIST</h1>
-
             <Paper>
                 <Box sx={sxRecipeContainer}>
                     {recipes.length > 0 ?
@@ -112,112 +96,32 @@ function RecipeList() {
                                             size="small">Start Recipe</Button>
                                         <Button size="small">Save Recipe</Button>
                                     </CardActions>
-                                    <CardContent>
+                                    <CardContent sx={sxCardContent}>
                                         <Typography
                                             sx={sxCardTitle}
                                             mt={-3}
                                             gutterBottom variant="h5" component="div">
                                             {recipe.recipe.label}
                                         </Typography>
-                                        <Box sx={sxBox}>
-                                            <Typography
-                                                mt={-2}
-                                                // overflow="scroll"
-                                                variant="body2" color="text.secondary">
-                                                <>
-                                                    {
-                                                        <ul>
-                                                            {recipe.recipe.ingredientLines.map(ingredient => (
-                                                                <li>{ingredient}</li>
-                                                            ))}
-                                                        </ul>
-                                                    }
-                                                </>
-                                            </Typography>
-                                        </Box>
+                                            <Box sx={sxBox}>
+                                                <Typography
+                                                    mt={0}
+                                                    variant="body2" color="text.secondary">
+                                                    <>
+                                                        {
+                                                            <ul>
+                                                                {recipe.recipe.ingredientLines.map(ingredient => (
+                                                                    <li>{ingredient}</li>
+                                                                ))}
+                                                            </ul>
+                                                        }
+                                                    </>
+                                                </Typography>
+                                            </Box>
                                     </CardContent>
                                 </Card>
-
-                                {/* COLLAPSABLE CARD BELOW */}
-                                {/* <RecipeListCard recipe={recipe}/> */}
-                                {/* <Card sx={{ maxWidth: 345 }}>
-                                    <CardHeader
-                                        avatar={
-                                            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                                                R
-                                            </Avatar>
-                                        }
-                                        action={
-                                            <IconButton aria-label="settings">
-                                                <MoreVertIcon />
-                                            </IconButton>
-                                        }
-                                        title="Shrimp and Chorizo Paella"
-                                        subheader="September 14, 2016"
-                                    />
-                                    <CardMedia
-                                        component="img"
-                                        height="194"
-                                        image="/static/images/cards/paella.jpg"
-                                        alt="Paella dish"
-                                    />
-                                    <CardContent>
-                                        <Typography variant="body2" color="text.secondary">
-                                            This impressive paella is a perfect party dish and a fun meal to cook
-                                            together with your guests. Add 1 cup of frozen peas along with the mussels,
-                                            if you like.
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions disableSpacing>
-                                        <IconButton aria-label="add to favorites">
-                                            <FavoriteIcon />
-                                        </IconButton>
-                                        <IconButton aria-label="share">
-                                            <ShareIcon />
-                                        </IconButton>
-                                        <ExpandMore
-                                            expand={expanded}
-                                            onClick={handleExpandClick}
-                                            aria-expanded={expanded}
-                                            aria-label="show more"
-                                        >
-                                            <ExpandMoreIcon />
-                                        </ExpandMore>
-                                    </CardActions>
-                                    <Collapse in={expanded} timeout="auto" unmountOnExit>
-                                        <CardContent>
-                                            <Typography paragraph>Method:</Typography>
-                                            <Typography paragraph>
-                                                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                                                aside for 10 minutes.
-                                            </Typography>
-                                            <Typography paragraph>
-                                                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                                                medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                                                occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                                                large plate and set aside, leaving chicken and chorizo in the pan. Add
-                                                pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                                                stirring often until thickened and fragrant, about 10 minutes. Add
-                                                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                                            </Typography>
-                                            <Typography paragraph>
-                                                Add rice and stir very gently to distribute. Top with artichokes and
-                                                peppers, and cook without stirring, until most of the liquid is absorbed,
-                                                15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                                                mussels, tucking them down into the rice, and cook again without
-                                                stirring, until mussels have opened and rice is just tender, 5 to 7
-                                                minutes more. (Discard any mussels that don’t open.)
-                                            </Typography>
-                                            <Typography>
-                                                Set aside off of the heat to let rest for 10 minutes, and then serve.
-                                            </Typography>
-                                        </CardContent>
-                                    </Collapse>
-                                </Card> */}
-
                             </>
                         ))
-
                         :
                         <p></p>}
                 </Box>
