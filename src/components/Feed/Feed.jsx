f98d52819d991b64cd3358bbe9aa51848473325c
Eric Meinzer <emeinzer7@gmail.com>
2022-01-09 12:51:23 -0600

    tool added to feed page working as expected; check key warnings

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -13,20 +13,57 @@ import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import IngredientAutocomplete from '../IngredientAutocomplete/IngredientAutocomplete';
+
+import Tooltip from '@mui/material/Tooltip';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+
+
+
+
+// -- sx Styles -- //
+import { sxSearchContainer } from '../Home/Home.style'
 
 function Feed() {
   const user = useSelector((store) => store.user);
   const recipes = useSelector(store => store.recipe);
   const dispatch = useDispatch();
   const ingredients = useSelector(store => store.ingredients);
+  const combo = useSelector(store => store.combo)
   const userCombos = useSelector(store => store.userCombos);
   const top5 = useSelector(store => store.top5);
+
+  const searchText = useSelector(store => store.ingredientSearch)
+  const pairings = useSelector(store => store.pairings)
+  const pairingOne = useSelector(store => store.comboPairingOne)
+  const pairingTwo = useSelector(store => store.comboPairingTwo)
+  const healthFilter = useSelector(store => store.healthFilter)
 
   useEffect(() => {
     dispatch({ type: 'FETCH_INGREDIENTS' });
     dispatch({ type: 'FETCH_COMBOS' });
     dispatch({ type: 'FETCH_TOP5' });
   }, [])
+
+  useEffect(() => {
+    switch (combo.length) {
+      case 1:
+        dispatch({ type: 'FETCH_INGREDIENT_ONE_PAIRINGS', payload: combo[0].id });
+        break;
+      case 2:
+        dispatch({ type: 'FETCH_INGREDIENT_TWO_PAIRINGS', payload: combo[1].id });
+        break;
+      case 3:
+        dispatch({
+          type: 'FETCH_RECIPES', payload: {
+            combo: combo,
+            filter: healthFilter
+          }
+        });
+        break;
+    }
+  }, [combo])
 
   const [newChallenge, setNewChallenge] = useState({ type: 'Combo of the Week', description: '', combo_id: '' })
 
@@ -42,6 +79,26 @@ function Feed() {
     setNewChallenge({ type: 'Combo of the Week', description: '', combo_id: '' })
     setOpen(true);
   };
+
+  // SEARCH function will capture first ingredient and then push you to the combo page to complete combo
+  const handleSearch = (searchText) => {
+
+    // let's dump the reducer so we know it's empty before starting a new combo;
+    dispatch({ type: 'CLEAR_COMBO_AND_RECIPE' })
+
+    console.log('CLICKED on handleSearch');
+    console.log('this is the searchText', searchText);
+
+    const searchedIngredientOne = ingredients.filter(ingredient => ingredient.name === searchText)
+    console.log('--- searchedIngredientOne:', searchedIngredientOne);
+
+    // ensures user only moves to combo page if they have selected from the list (Must Select From The List!)
+    if (searchedIngredientOne.length === 0) {
+      return alert('Make a selection from the list')
+    }
+    // put ingredient into the reducer
+    dispatch({ type: 'SET_COMBO_INGREDIENT', payload: searchedIngredientOne[0] })
+  }
 
   let topFiveIngredients = []
   for (let ingredient of ingredients) {
@@ -111,19 +168,99 @@ function Feed() {
 
   console.log('Challenge', newChallenge);
   console.log(topFiveIngredients);
+
+  const sxIngredientContainer = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    // border: '1px solid red',
+    gap: 2,
+    justifyContent: 'center',
+    mb: 2,
+  }
+
+  const sxPhotoBox = {
+    // border: '1px solid red',
+    width: 35,
+    height: 35,
+    boxShadow: 3,
+    my: 2,
+    ml: 1,
+    borderRadius: '50%',
+    // mx: 'auto',
+  };
+
+  const sxCardContent = {
+    // border: '1px solid red',
+    display: 'flex',
+    justifyContent: 'left',
+    // gap: 2,
+    alignItems: 'center',
+    width: 250,
+    height: 50,
+    borderRadius: 5,
+  }
+
+  const sxSuperComboCardContent = {
+    border: '5px solid #00d61d',
+    display: 'flex',
+    justifyContent: 'left',
+    // gap: 2,
+    alignItems: 'center',
+    width: 250,
+    height: 50,
+    borderRadius: 5,
+  }
+
+  const sxCardTypography = {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+  }
+
+  const sxTooltip = {
+    borderRadius: 5,
+  }
+
+
+
+
+  // superCombo is filters the two ingredient pairings and creates new array with duplicates
+  let superCombo = pairingOne.filter(o1 => pairingTwo.some(o2 => o1.id === o2.id));
+
+  const pairingOneIds = new Set(pairingOne.map(({ id }) => id));
+  const superComboIds = new Set(superCombo.map(({ id }) => id));
+
+  // filter any duplicate ingredients out of two pairing lists
+  let combined = [
+    ...pairingOne,
+    ...pairingTwo.filter(({ id }) => !pairingOneIds.has(id))
+  ];
+
+  // filter out super combo ingredients from combined pairing list
+  combined = [
+    ...combined.filter(({ id }) => !superComboIds.has(id))
+  ]
+
+  // filter out ingredients in combo tool from ingredient listing
+  if (combo.length === 2) {
+    combined = combined.filter(ingredient => ingredient.id != combo[0].id).filter(ingredient => ingredient.id != combo[1].id);
+  }
+
+
   return (
     <>
       <Box sx={sxMetrics}>
-        <Typography variant='h4' sx={{mb: 4}}>App Wide User Metrics</Typography>
+        <Typography variant='h4' sx={{ mb: 4 }}>App Wide User Metrics</Typography>
         <Box sx={sxMetrics}>
           <Grid container spacing={2}>
             <Grid item xs={4}>
               <Item>Top 5 Ingredients Used:
-                <Typography sx={{fontSize: 12}}>{topFiveIngredients[0]?.name} ({top5[0]?.times_used})</Typography>
-                <Typography sx={{fontSize: 12}}>{topFiveIngredients[1]?.name} ({top5[1]?.times_used})</Typography>
-                <Typography sx={{fontSize: 12}}>{topFiveIngredients[2]?.name} ({top5[2]?.times_used})</Typography>
-                <Typography sx={{fontSize: 12}}>{topFiveIngredients[3]?.name} ({top5[3]?.times_used})</Typography>
-                <Typography sx={{fontSize: 12}}>{topFiveIngredients[4]?.name} ({top5[4]?.times_used})</Typography>
+                <Typography sx={{ fontSize: 12 }}>{topFiveIngredients[0]?.name} ({top5[0]?.times_used})</Typography>
+                <Typography sx={{ fontSize: 12 }}>{topFiveIngredients[1]?.name} ({top5[1]?.times_used})</Typography>
+                <Typography sx={{ fontSize: 12 }}>{topFiveIngredients[2]?.name} ({top5[2]?.times_used})</Typography>
+                <Typography sx={{ fontSize: 12 }}>{topFiveIngredients[3]?.name} ({top5[3]?.times_used})</Typography>
+                <Typography sx={{ fontSize: 12 }}>{topFiveIngredients[4]?.name} ({top5[4]?.times_used})</Typography>
               </Item>
             </Grid>
             <Grid item xs={4}>
@@ -137,13 +274,26 @@ function Feed() {
       </Box>
       <br />
       <Box sx={sxFeaturedCombo}>
-        <Typography variant='h4' sx={{ textAlign: "center", my: 4}}>Create Featured Combo</Typography>
+        <Typography variant='h4' sx={{ textAlign: "center", my: 4 }}>Create Featured Combo</Typography>
+
+        {/* ingredient search */}
+        {combo.length === 0 &&
+          <>
+            <Typography sx={sxSearchContainer}
+              variant="h5">Find Your First Ingredient</Typography>
+            <Box sx={sxSearchContainer}>
+
+              <IngredientAutocomplete />
+              <Button onClick={() => handleSearch(searchText)} variant="outlined">search</Button>
+            </Box>
+          </>
+        }
 
         {/* combo selector */}
         <ComboTool />
 
         {/* list of ingredients from DB */}
-        <ul>
+        {/* <ul>
           {ingredients.map((ingredient) => {
             return (
               <li
@@ -152,8 +302,80 @@ function Feed() {
                 onClick={() => dispatch({ type: 'SET_COMBO_INGREDIENT', payload: ingredient })}>{ingredient.name}</li>
             )
           })}
-        </ul>
-        <br />
+        </ul> */}
+        {/* <br /> */}
+
+
+
+
+        {/* super combo ingredients list */}
+
+        {superCombo.length > 0 && combo.length < 3 &&
+          <>
+            <Typography sx={sxIngredientContainer}>Super Combos</Typography>
+            <Box sx={sxIngredientContainer}>
+              {superCombo.map(ingredient => (
+                <Tooltip sx={sxTooltip}
+                  title={
+                    <>
+                      <Typography
+                        variant="body1">{ingredient.description}</Typography>
+                    </>
+                  }
+                >
+                  <Card elevation={3}
+                    onClick={() => dispatch({ type: 'SET_COMBO_INGREDIENT', payload: ingredient })}
+                    sx={sxSuperComboCardContent}>
+                    <CardMedia
+                      sx={sxPhotoBox}
+                      component="img"
+                      image={ingredient.pic}
+                      alt={ingredient.name}
+                    />
+                    <Typography
+                      sx={sxCardTypography}
+                      gutterBottom variant="body1" component="div">
+                      {ingredient.name}
+                    </Typography>
+                  </Card>
+                </Tooltip>
+              ))}
+            </Box>
+          </>
+        }
+
+        {/* combined ingredient listing with no duplicates */}
+        {combo.length > 0 && combo.length < 3 &&
+          <Box sx={sxIngredientContainer}>
+            {combined.map(ingredient => (
+              <Tooltip sx={sxTooltip}
+                title={
+                  <>
+                    <Typography variant="body1">{ingredient.description}</Typography>
+                  </>
+                }
+              >
+                <Card elevation={3}
+                  onClick={() => dispatch({ type: 'SET_COMBO_INGREDIENT', payload: ingredient })}
+                  sx={sxCardContent}>
+                  <CardMedia
+                    sx={sxPhotoBox}
+                    component="img"
+                    image={ingredient.pic}
+                    alt={ingredient.name}
+                  />
+                  <Typography
+                    sx={sxCardTypography}
+                    gutterBottom variant="body1" component="div">
+                    {ingredient.name}
+                  </Typography>
+                </Card>
+              </Tooltip>
+            ))}
+          </Box>
+        }
+
+
 
         <Typography sx={{ textAlign: "center" }}>Saved Flavor Combos</Typography>
         <Box sx={sxFlavorCombos}>
